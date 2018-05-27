62330edeb28f0191d5748034530820503c21ea85
Jason Neal <jason.neal@astro.up.pt>
2018-05-27 12:43:03 +0100

    Initialize with toga cookie-cutter.

---
+++ pydice/app.py
@@ -0,0 +1,27 @@
+
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+from random import random
+
+class Pydice(toga.App):
+    self.nsides = 6
+    self.ndice = 2
+
+    def startup(self):
+        # Create a main window with a name matching the app
+        self.main_window = toga.MainWindow(title=self.name)
+
+        # Create a main content box
+        main_box = toga.Box()
+
+        # Add the content on the main window
+        self.main_window.content = main_box
+
+        # Show the main window
+        self.main_window.show()
+
+
+def main():
+    return Pydice('pydice', 'com.example.pydice')
+
